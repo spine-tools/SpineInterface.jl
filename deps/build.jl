402fdce180c647b78bb27763cb2d698b284e2451
Manuel Marin <manuelma@kth.se>
2020-02-19 10:41:24 -0400

    Don't autoinstall spinedb_api anymore, but give the user options
    The main driver for this is that autoinstallation in the build step
    with QProcess from spinetoolbox fails with segmentation fault.
    But then again, it doesn't feel right to autoinstall if the user
    is going to switch to another Python later.

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,30 @@
 using PyCall
 
-python = PyCall.pyprogramname
-run(`$python -m pip install --user 'git+https://github.com/Spine-project/Spine-Database-API'`)
+try
+    copy!(db_api, pyimport("spinedb_api"))
+catch err
+    if err isa PyCall.PyError
+        error(
+            """
+            The required Python package `spinedb_api` could not be found in the current Python environment
+
+                $(PyCall.pyprogramname)
+
+            You can fix this in two different ways:
+
+            A. Install `spinedb_api` in the current Python environment; open a terminal (command prompt on Windows) and run
+
+                $(PyCall.pyprogramname) -m pip install --user 'git+https://github.com/Spine-project/Spine-Database-API'
+
+            B. Switch to another Python environment that has `spinedb_api` installed; from Julia, run 
+
+                ENV["PYTHON"] = "... path of the python executable ..."
+                Pkg.build("PyCall")
+
+            And restart Julia.
+            """
+        )
+    else
+        rethrow()
+    end
+end
--- src/SpineInterface.jl
+++ src/SpineInterface.jl
@@ -45,12 +45,53 @@ export anything
 
 
 function __init__()
-    copy!(db_api, pyimport("spinedb_api"))
+    try
+        copy!(db_api, pyimport("spinedb_api"))
+    catch err
+        if err isa PyCall.PyError
+            error(
+                """
+                The required Python package `spinedb_api` could not be found in the current Python environment
+
+                    $(PyCall.pyprogramname)
+
+                You can fix this in two different ways:
+
+                A. Install `spinedb_api` in the current Python environment; open a terminal (command prompt on Windows) and run
+
+                    $(PyCall.pyprogramname) -m pip install --user 'git+https://github.com/Spine-project/Spine-Database-API'
+
+                B. Switch to another Python environment that has `spinedb_api` installed; from Julia, run 
+
+                    ENV["PYTHON"] = "... path of the python executable ..."
+                    Pkg.build("PyCall")
+
+                And restart Julia.
+                """
+            )
+        else
+            rethrow()
+        end
+    end
     current_version = VersionNumber(db_api.__version__)
     current_version >= required_spinedb_api_version || error(
         """
-        SpineInterface couldn't find the required version of `spinedb_api` and needs to be rebuilt:
-        Please run `import Pkg; Pkg.build("SpineInterface")` to rebuild SpineInterface.
+        The required version of `spinedb_api` could not be found in the current Python environment
+
+            $(PyCall.pyprogramname)
+            
+        You can fix this in two different ways:
+
+        A. Upgrade `spinedb_api` to its latest version in the current Python environment; open a terminal (command prompt on Windows) and run
+
+            $(PyCall.pyprogramname) -m pip upgrade --user 'git+https://github.com/Spine-project/Spine-Database-API'
+
+        B. Switch to another Python environment that has the latest version of `spinedb_api` installed; from Julia, run 
+
+            ENV["PYTHON"] = "... path of the python executable ..."
+            Pkg.build("PyCall")
+
+        And restart Julia.
         """
     )
     py"""
