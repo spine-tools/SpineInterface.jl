7100b2562d3f582ebbc705f267b106205f81ebb8
Manuel Marin <manuelma@kth.se>
2020-03-03 15:43:51 -0400

    Try to optimize `Call` api
    The Call api is unbearably slow, this commit is a WIP towards making
    it faster. We manage to reduce allocations but it feels like we need
    to keep working.
    
    For the record, the current strategy is to make `Call` an abstract
    type, and then provide optimized (?) subtypes for Parameter,
    general Function, and scalar.

--- src/call.jl
+++ src/call.jl
@@ -17,66 +17,71 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #############################################################################
 
-struct Call
-    func
-    args
-    kwargs
-    _is_dynamic::Bool
-    function Call(func, args, kwargs)
-        _is_dynamic = any(is_dynamic(arg) for arg in (args..., kwargs...))
-        new(func, args, kwargs, _is_dynamic)
-    end
+abstract type Call end
+
+struct IdentityCall{T} <: Call
+    value::T
+end
+
+struct OperatorCall <: Call
+    operator::Function
+    args::Tuple
+end
+
+struct ParameterCall <: Call
+    parameter::Parameter
+    kwargs::NamedTuple
 end
 
 # Outer constructors
 Call(other::Call) = copy(other)
-_identity(x) = x
-Call(n) = Call(_identity, (n,), ())
-Base.show(::IO, ::typeof(_identity)) = nothing
+Call(n) = IdentityCall(n)
+Call(op::Function, args::Tuple) = OperatorCall(op, args)
+Call(param::Parameter, kwargs::NamedTuple) = ParameterCall(param, kwargs)
 
-# utility
-Base.copy(c::Call) = Call(c.func, c.args, c.kwargs)
-Base.convert(::Type{Call}, x::T) where {T<:Real} = Call(x)
-function Base.show(io::IO, call::Call)
-    call_str = string(call.func)
-    args_str = join(call.args, ", ")
-    kwargs_str = join([join(kw, "=") for kw in pairs(call.kwargs)], ", ")
-    args_str = join(filter(!isempty, [args_str, kwargs_str]), "; ")
-    print(io, string(call.func, "(", args_str, ")"))
-end
-
-# realize
+# api
 realize(x) = x
-
-function realize(call::Call)
-    args = (realize(arg) for arg in call.args)
-    kwargs = (k => realize(v) for (k, v) in pairs(call.kwargs))
-    call.func(args...; kwargs...)
-end
+realize(call::ParameterCall) = call.parameter(; call.kwargs...)
+realize(call::OperatorCall) = call.operator(realize.(call.args)...)
+realize(call::IdentityCall) = call.value
 
 is_dynamic(x) = false
 is_dynamic(x::TimeSlice) = true
-is_dynamic(call::Call) = call._is_dynamic
+is_dynamic(call::ParameterCall) = any(is_dynamic(kwarg) for kwarg in call.kwargs)
+is_dynamic(call::OperatorCall) = any(is_dynamic(arg) for arg in call.args)
+is_dynamic(call::IdentityCall) = is_dynamic(call.value)
+
+# Base
+Base.copy(c::ParameterCall) = ParameterCall(c.parameter, c.kwargs)
+Base.copy(c::OperatorCall) = OperatorCall(c.operator, c.args)
+Base.copy(c::IdentityCall) = IdentityCall(c.value)
+Base.convert(::Type{Call}, x::T) where {T<:Real} = IdentityCall(x)
+Base.show(io::IO, call::IdentityCall) = print(io, call.value)
+Base.show(io::IO, call::OperatorCall) = print(io, join(call.args, string(" ", call.operator, " ")))
+function Base.show(io::IO, call::ParameterCall)
+    kwargs_str = join([join(kw, "=") for kw in pairs(call.kwargs)], ", ")
+    print(io, string(call.parameter, "(", kwargs_str, ")"))
+end
 
 # operators
-Base.zero(::Type{Call}) where T = Call(0.0)
-Base.zero(::Call) = Call(0.0)
-Base.one(::Type{Call}) = Call(1.0)
-Base.one(::Call) = Call(1.0)
+Base.zero(::Type{T}) where T<:Call = IdentityCall(0.0)
+Base.zero(::Call) = IdentityCall(0.0)
+Base.one(::Type{Call}) = IdentityCall(1.0)
+Base.one(::Call) = IdentityCall(1.0)
+Base.:+(x::Call, y::Call) = OperatorCall(+, (x, y))
+Base.:+(x::Call, y) = OperatorCall(+, (x, y))
+Base.:+(x, y::Call) = OperatorCall(+, (x, y))
 Base.:+(x::Call) = x
-Base.:+(x::Call, y) = Call(+, (x, y), ())
-Base.:+(x, y::Call) = Call(+, (x, y), ())
-Base.:+(x::Call, y::Call) = Call(+, (x, y), ())
-Base.:-(x::Call) = Call(-, (0.0, x), ())
-Base.:-(x::Call, y) = Call(-, (x, y), ())
-Base.:-(x, y::Call) = Call(-, (x, y), ())
-Base.:-(x::Call, y::Call) = Call(-, (x, y), ())
-Base.:*(x::Call, y) = Call(*, (x, y), ())
-Base.:*(x, y::Call) = Call(*, (x, y), ())
-Base.:*(x::Call, y::Call) = Call(*, (x, y), ())
-Base.:/(x::Call, y) = Call(/, (x, y), ())
-Base.:/(x, y::Call) = Call(/, (x, y), ())
-Base.:/(x::Call, y::Call) = Call(/, (x, y), ())
+Base.:-(x::Call, y::Call) = OperatorCall(-, (x, y))
+Base.:-(x::Call, y) = OperatorCall(-, (x, y))
+Base.:-(x, y::Call) = OperatorCall(-, (x, y))
+Base.:-(x::Call) = (-)(0.0, x)
+Base.:*(x::Call, y::Call) = OperatorCall(*, (x, y))
+Base.:*(x::Call, y) = OperatorCall(*, (x, y))
+Base.:*(x, y::Call) = OperatorCall(*, (x, y))
+Base.:/(x::Call, y::Call) = OperatorCall(/, (x, y))
+Base.:/(x::Call, y) = OperatorCall(/, (x, y))
+Base.:/(x, y::Call) = OperatorCall(/, (x, y))
 
 macro call(expr::Expr)
     # TODO: finish this
@@ -90,4 +95,4 @@ macro call(expr::Expr)
 end
 
 # Override `getindex` for `Parameter` so we can call `parameter[...]` and get a `Call`
-Base.getindex(parameter::Parameter, inds::NamedTuple) = Call(parameter, (), inds)
+Base.getindex(parameter::Parameter, inds::NamedTuple) = ParameterCall(parameter, inds)
