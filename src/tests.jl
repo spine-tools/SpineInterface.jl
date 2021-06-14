b2402a59164e3bd5bd7229a183a117d0f0be7542
Topi Rasku <topi.rasku@vtt.fi>
2021-06-14 11:39:40 +0300

    Bugfixes and clarifying the terminology and messages.

--- src/tests.jl
+++ src/tests.jl
@@ -1,7 +1,9 @@
 #=
     tests.jl
 
-This file contains functions for ensuring the archetype building definitions are valid.
+This file contains functions for testing `Parameter`, `ObjectClass`, and `RelationshipClass` entities.
+The primary purpose is to make writing database tests easier, e.g. ensuring that all values of a `Parameter` are
+of a certain type, or withing a specified range for values.
 =#
 
 """
@@ -17,13 +19,17 @@ end
 
 """
     test_parameter(
-        param::Parameter, object_class::ObjectClass; value_type=nothing, value_min::Real=-Inf, value_max::Real=Inf
+        param::Parameter; value_type=nothing, value_min::Real=-Inf, value_max::Real=Inf
     )
 
-Test if `param` value has the expected `DataType` and/or is contained between `value_min`, and `value_max` for each `object`.
-
-A method is provided for also testing `RelationshipClass` parameters.
+Test if `param` value has the expected `DataType` and/or is contained between `value_min`, and `value_max`.
+Methods are provided for testing `ObjectClass` and `RelationshipClass` separately.
 """
+function test_parameter(param::Parameter; value_type=nothing, value_min::Real=-Inf, value_max::Real=Inf)
+    for class in param.classes
+        test_parameter(param, class; value_type=value_type, value_min=value_min, value_max=value_max)
+    end
+end
 function test_parameter(
     param::Parameter, object_class::ObjectClass; value_type=nothing, value_min::Real=-Inf, value_max::Real=Inf
 )
@@ -34,13 +40,13 @@ function test_parameter(
             if !isnothing(value_type)
                 @test _check(
                     object_class.parameter_values[object][param.name].value isa value_type,
-                    "Unexpected type for `$(param)` for `$(object)` in `$(object_class)`! `$(value_type)` expected."
+                    "Unexpected `$(param)` type for `$(object)` - `$(value_type)` expected!"
                 )
             end
-            if value_type isa Real
+            if value_type <: Real
                 @test _check(
                     value_min <= object_class.parameter_values[object][param.name].value <= value_max,
-                    "`$(param)` for `$(object)` in `$(object_class)` outside expected range `[$(value_min),$(value_max)]`!"
+                    "`$(param)` for `$(object)` outside expected range `[$(value_min),$(value_max)]`!"
                 )
             end
         end
@@ -56,13 +62,13 @@ function test_parameter(
             if !isnothing(value_type)
                 @test _check(
                     relationship_class.parameter_values[tuple(relationship...)][param.name].value isa value_type,
-                    "Unexpected type for `$(param)` for `$(relationship)` in `$(relationship_class)`! `$(value_type)` expected."
+                    "Unexpected `$(param)` type for `$(relationship)` - `$(value_type)` expected!"
                 )
             end
-            if value_type isa Real
+            if value_type <: Real
                 @test _check(
                     value_min <= relationship_class.parameter_values[tuple(relationship...)][param.name].value <= value_max,
-                    "`$(param)` for `$(relationship)` in `$(relationship_class)` outside expected range `[$(value_min),$(value_max)]`!"
+                    "`$(param)` for `$(relationship)` outside expected range `[$(value_min),$(value_max)]`!"
                 )
             end
         end
@@ -71,13 +77,13 @@ end
 
 
 """
-    test_object_class(object_class::ObjectClass; included_in=nothing, n_entries_min::Real=0, n_entries_max::Real=Inf)
+    test_object_class(object_class::ObjectClass; included_in=nothing, count_min::Real=0, count_max::Real=Inf)
 
-Test if the `object_class` is `included_in` the desired `RelationshipClasses` with a desired number of entries for each `object`.
+Test if the `object_class` is `included_in` the desired `RelationshipClasses` with a desired entry count for each `object`.
 """
-function test_object_class(object_class::ObjectClass; included_in=nothing, n_entries_min::Real=0, n_entries_max::Real=Inf)
+function test_object_class(object_class::ObjectClass; included_in=nothing, count_min::Real=0, count_max::Real=Inf)
     @testset """
-    Testing `$(object_class)` in `$(included_in)` with entry numbers within `[$(n_entries_min),$(n_entries_max)]`.
+    Testing `$(object_class)` in `$(included_in)` and entry count within `[$(count_min),$(count_max)]`.
     """ begin
         if !isnothing(included_in)
             obs_in_rels = getfield.(included_in.relationships, object_class.name)
@@ -87,8 +93,8 @@ function test_object_class(object_class::ObjectClass; included_in=nothing, n_ent
             )
             for object in object_class.objects
                 @test _check(
-                    n_entries_min <= count(entry -> entry == object, obs_in_rels) <= n_entries_max,
-                    "Number of `$(object)` relationships in `$(included_in)` not within desired range `[$(n_entries_min),$(n_entries_max)]`!"
+                    count_min <= count(entry -> entry == object, obs_in_rels) <= count_max,
+                    "`$(object)` count in `$(included_in)` not within `[$(count_min),$(count_max)]`!"
                 )
             end
         else
@@ -101,16 +107,16 @@ end
 
 """
     test_relationship_class(
-        relationship_class:RelationshipClass; included_in=nothing, n_entries_min::Real=0, n_entries_max::Real=Inf
+        relationship_class:RelationshipClass; included_in=nothing, count_min::Real=0, count_max::Real=Inf
     )
 
 Test if `relationship_class` is `included_in` another `RelationshipClass` with the desired number of entries.
 """
 function test_relationship_class(
-    relationship_class::RelationshipClass; included_in=nothing, n_entries_min::Real=0, n_entries_max::Real=Inf
+    relationship_class::RelationshipClass; included_in=nothing, count_min::Real=0, count_max::Real=Inf
 )
 @testset """
-    Testing `$(relationship_class)` in `$(included_in)` with entry numbers within `[$(n_entries_min),$(n_entries_max)]`.
+    Testing `$(relationship_class)` in `$(included_in)` entry count within `[$(count_min),$(count_max)]`.
     """ begin
         if !isnothing(included_in)
             rel_in_rels = zip(
@@ -121,8 +127,8 @@ function test_relationship_class(
             )
             for rel in values.(relationship_class.relationships)
                 @test _check(
-                    n_entries_min <= count(entry -> entry==rel, rel_in_rels) <= n_entries_max,
-                    "Number of `$(rel)` relationships in `$(included_in)` not within desired range `[$(n_entries_min),$(n_entries_max)]`!"
+                    count_min <= count(entry -> entry==rel, rel_in_rels) <= count_max,
+                    "`$(rel)` count in `$(included_in)` not within `[$(count_min),$(count_max)]`!"
                 )
             end
         else
