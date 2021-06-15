524787a854b80c97846679c0dca3277c5b4a62b0
Topi Rasku <topi.rasku@vtt.fi>
2021-06-15 14:54:12 +0300

    Testing functions now consider default values.

--- src/tests.jl
+++ src/tests.jl
@@ -35,21 +35,26 @@ function test_parameter(
     end
 end
 function test_parameter(
-    param::Parameter, object_class::ObjectClass, value_type::DataType;
+    param::Parameter, obj_class::ObjectClass, value_type::DataType;
     value_min::Real=-Inf, value_max::Real=Inf, limit::Real=Inf
 )
     @testset """
-    Testing parameter `$(param)` for `$(object_class)` for `$(value_type)` within `[$(value_min),$(value_max)]`.
+    Testing parameter `$(param)` for `$(obj_class)` for `$(value_type)` within `[$(value_min),$(value_max)]`.
     """ begin
-        for (i,object) in enumerate(object_class.objects)
+        for (i,object) in enumerate(obj_class.objects)
             if i <= limit
+                val = _get(
+                    obj_class.parameter_values[object],
+                    param.name,
+                    obj_class.parameter_defaults[param.name]
+                ).value
                 @test _check(
-                    object_class.parameter_values[object][param.name].value isa value_type,
+                    val isa value_type,
                     "Unexpected `$(param)` type for `$(object)` - `$(value_type)` expected!"
                 )
                 if value_type <: Real
                     @test _check(
-                        value_min <= object_class.parameter_values[object][param.name].value <= value_max,
+                        value_min <= val <= value_max,
                         "`$(param)` for `$(object)` outside expected range `[$(value_min),$(value_max)]`!"
                     )
                 end
@@ -59,21 +64,26 @@ function test_parameter(
     end
 end
 function test_parameter(
-    param::Parameter, relationship_class::RelationshipClass, value_type::DataType;
+    param::Parameter, rel_class::RelationshipClass, value_type::DataType;
     value_min::Real=-Inf, value_max::Real=Inf, limit::Real=Inf
 )
     @testset """
-    Testing parameter `$(param)` for `$(relationship_class)` for `$(value_type)` within `[$(value_min),$(value_max)]`.
+    Testing parameter `$(param)` for `$(rel_class)` for `$(value_type)` within `[$(value_min),$(value_max)]`.
     """ begin
-        for (i,relationship) in enumerate(relationship_class.relationships)
+        for (i,relationship) in enumerate(rel_class.relationships)
             if i <= limit
+                val = _get(
+                    rel_class.parameter_values[tuple(relationship...)],
+                    param.name,
+                    rel_class.parameter_defaults[param.name]
+                ).value
                 @test _check(
-                    relationship_class.parameter_values[tuple(relationship...)][param.name].value isa value_type,
+                    val isa value_type,
                     "Unexpected `$(param)` type for `$(relationship)` - `$(value_type)` expected!"
                 )
                 if value_type <: Real
                     @test _check(
-                        value_min <= relationship_class.parameter_values[tuple(relationship...)][param.name].value <= value_max,
+                        value_min <= val <= value_max,
                         "`$(param)` for `$(relationship)` outside expected range `[$(value_min),$(value_max)]`!"
                     )
                 end
@@ -86,7 +96,7 @@ end
 
 """
     test_object_class(
-        object_class::ObjectClass, relationship_class::RelationshipClass;
+        obj_class::ObjectClass, rel_class::RelationshipClass;
         count_min::Real=0, count_max::Real=Inf, limit::Real=Inf
     )
 
@@ -94,19 +104,19 @@ Test if the `object_class` is included in `relationship_class` with a desired en
 The `limit` keyword can be used to limit the number of tests performed.
 """
 function test_object_class(
-    object_class::ObjectClass, relationship_class::RelationshipClass;
+    obj_class::ObjectClass, rel_class::RelationshipClass;
     count_min::Real=0, count_max::Real=Inf, limit::Real=Inf
 )
     @testset """
-    Testing `$(object_class)` in `$(relationship_class)` and entry count within `[$(count_min),$(count_max)]`.
+    Testing `$(obj_class)` in `$(rel_class)` and entry count within `[$(count_min),$(count_max)]`.
     """ begin
-        obs_in_rels = getfield.(relationship_class.relationships, object_class.name)
-        isempty(obs_in_rels) && error("`$(object_class)` not included in `$(relationship_class)`!")
-        for (i,object) in enumerate(object_class.objects)
+        obs_in_rels = getfield.(rel_class.relationships, obj_class.name)
+        isempty(obs_in_rels) && error("`$(obj_class)` not included in `$(rel_class)`!")
+        for (i,object) in enumerate(obj_class.objects)
             if i <= limit
                 @test _check(
                     count_min <= count(entry -> entry == object, obs_in_rels) <= count_max,
-                    "`$(object)` count in `$(relationship_class)` not within `[$(count_min),$(count_max)]`!"
+                    "`$(object)` count in `$(rel_class)` not within `[$(count_min),$(count_max)]`!"
                 )
             else break
             end
@@ -117,7 +127,7 @@ end
 
 """
     test_relationship_class(
-        relationship_class:RelationshipClass, in_rel_class::RelationshipClass;
+        rel_class:RelationshipClass, in_rel_class::RelationshipClass;
         count_min::Real=0, count_max::Real=Inf, limit::Real=Inf
     )
 
@@ -125,15 +135,15 @@ Test if `relationship_class` is included in `in_rel_class` with the desired numb
 The `limit` keyword can be used to limit the number of tests performed.
 """
 function test_relationship_class(
-    relationship_class::RelationshipClass, in_rel_class::RelationshipClass;
+    rel_class::RelationshipClass, in_rel_class::RelationshipClass;
     count_min::Real=0, count_max::Real=Inf, limit::Real=Inf
 )
 @testset """
-    Testing `$(relationship_class)` in `$(in_rel_class)` entry count within `[$(count_min),$(count_max)]`.
+    Testing `$(rel_class)` in `$(in_rel_class)` entry count within `[$(count_min),$(count_max)]`.
     """ begin
-        fields = intersect(relationship_class.object_class_names, in_rel_class.object_class_names)
-        isempty(fields) && error("`$(relationship_class)` and `$(in_rel_class)` have no common `ObjectClasses`!")
-        rels = zip([getfield.(relationship_class.relationships, field) for field in fields]...)
+        fields = intersect(rel_class.object_class_names, in_rel_class.object_class_names)
+        isempty(fields) && error("`$(rel_class)` and `$(in_rel_class)` have no common `ObjectClasses`!")
+        rels = zip([getfield.(rel_class.relationships, field) for field in fields]...)
         in_rels = zip([getfield.(in_rel_class.relationships, field) for field in fields]...)
         for (i,rel) in enumerate(rels)
             if i <= limit
