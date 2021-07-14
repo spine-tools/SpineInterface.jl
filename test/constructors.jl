bcac034dedb1bdad9255e6fb3b2e55478b933274
Rasku Topi <topi.rasku@vtt.fi>
2021-07-14 13:46:18 +0300

    Improve comment

--- test/constructors.jl
+++ test/constructors.jl
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #############################################################################
 
-# Initialise an in-memory database to avoid `StackOverflowError`???
+# Initialise an in-memory database to avoid `StackOverflowError` when running this file solo???
 using_spinedb("sqlite://")
 @testset "constructors" begin
     ducks = [Object(:Daffy), Object(:Donald)]
