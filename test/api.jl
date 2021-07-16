9daf2348e45a81cfb8abb70f5345407f7b9aa2e4
Rasku Topi <topi.rasku@vtt.fi>
2021-07-16 10:42:51 +0300

    Add unit tests to ensure `TimePattern` behaves as intended with `DateTime` arguments.

--- test/api.jl
+++ test/api.jl
@@ -160,13 +160,23 @@ end
     end
     @testset "time_pattern" begin
         isfile(path) && rm(path)
-        val = Dict(SpineInterface.PeriodCollection("D1-5") => 30.5, SpineInterface.PeriodCollection("D6-7") => 24.7)
+        val = Dict(SpineInterface.PeriodCollection("D2-5") => 30.5, SpineInterface.PeriodCollection("D6-7") => 24.7)
         @test val isa SpineInterface.TimePattern
         parameters = Dict(:apero_time => Dict((country=:France,) => val))
         write_parameters(parameters, url)
         using_spinedb(url)
-        @test apero_time(country=country(:France), t=TimeSlice(DateTime(0, 1, 3), DateTime(0, 1, 5))) == 30.5
+        @test isnothing(apero_time(country=country(:France), t=DateTime(0, 1, 1)))
+        @test isnothing(apero_time(country=country(:France), t=TimeSlice(DateTime(0, 1, 1), DateTime(0, 1, 1, 23))))
+        @test apero_time(country=country(:France), t=TimeSlice(DateTime(0, 1, 1), DateTime(0, 1, 5))) == 30.5
+        @test apero_time(country=country(:France), t=DateTime(0, 1, 2)) == 30.5
+        @test apero_time(country=country(:France), t=DateTime(0, 1, 5)) == 30.5
+        @test apero_time(country=country(:France), t=TimeSlice(DateTime(0, 1, 5), DateTime(0, 1, 6))) == (30.5 + 24.7) / 2.
         @test apero_time(country=country(:France), t=TimeSlice(DateTime(0, 1, 6), DateTime(0, 1, 6, 10))) == 24.7
+        @test apero_time(country=country(:France), t=DateTime(0, 1, 6)) == 24.7
+        @test apero_time(country=country(:France), t=TimeSlice(DateTime(0, 1, 7), DateTime(0, 1, 8))) == 24.7
+        @test apero_time(country=country(:France), t=DateTime(0, 1, 7)) == 24.7
+        @test isnothing(apero_time(country=country(:France), t=TimeSlice(DateTime(0, 1, 8), DateTime(0, 1, 31))))
+        @test isnothing(apero_time(country=country(:France), t=DateTime(0, 1, 8)))
     end
     @testset "time_series" begin
         isfile(path) && rm(path)
