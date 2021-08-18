e8fdb4d13e07df520dde5c5c5529952caf02340a
Topi Rasku <topi.rasku@vtt.fi>
2021-08-18 12:51:20 +0300

    Reverting some previous changes.
    Turns out that calling time-varying data with `DateTime` cannot be handled via `TimeSlice` after all, so I'll have to revert that.

--- src/util.jl
+++ src/util.jl
@@ -112,7 +112,6 @@ end
 
 (p::TimePatternParameterValue)(; t::Union{DateTime,TimeSlice,Nothing}=nothing, kwargs...) = p(t)
 (p::TimePatternParameterValue)(::Nothing) = p.value
-(p::TimePatternParameterValue)(t::DateTime) = p(TimeSlice(t, t))
 function (p::TimePatternParameterValue)(t::TimeSlice)
     vals = [val for (tp, val) in p.value if overlaps(t, tp)]
     isempty(vals) && return nothing
@@ -120,7 +119,7 @@ function (p::TimePatternParameterValue)(t::TimeSlice)
 end
 
 function _search_overlap(ts::TimeSeries, t_start::DateTime, t_end::DateTime)
-    (t_start <= ts.indexes[end] && t_end >= ts.indexes[1]) || return ()
+    (t_start <= ts.indexes[end] && t_end > ts.indexes[1]) || return ()
     a = max(1, searchsortedlast(ts.indexes, t_start))
     b = searchsortedfirst(ts.indexes, t_end) - 1
     (a, b)
@@ -128,26 +127,23 @@ end
 
 (p::StandardTimeSeriesParameterValue)(; t::Union{DateTime,TimeSlice,Nothing}=nothing, kwargs...) = p(t)
 (p::StandardTimeSeriesParameterValue)(::Nothing) = p.value
-(p::StandardTimeSeriesParameterValue)(t::DateTime) = p(TimeSlice(t, t))
 function (p::StandardTimeSeriesParameterValue)(t::TimeSlice)
     p.value.ignore_year && (t -= Year(start(t)))
     ab = _search_overlap(p.value, start(t), end_(t))
     isempty(ab) && return nothing
     a, b = ab
-    a > b ? b = a : nothing
     vals = Iterators.filter(!isnan, p.value.values[a:b])
     mean(vals)
 end
 
 (p::RepeatingTimeSeriesParameterValue)(; t::Union{DateTime,TimeSlice,Nothing}=nothing, kwargs...) = p(t)
 (p::RepeatingTimeSeriesParameterValue)(::Nothing) = p.value
-(p::RepeatingTimeSeriesParameterValue)(t::DateTime) = p(TimeSlice(t, t))
 function (p::RepeatingTimeSeriesParameterValue)(t::TimeSlice)
     t_start = start(t)
     t_end = end_(t)
     p.value.ignore_year && (t_start -= Year(t_start))
     mismatch = t_start - p.value.indexes[1]
-    reps = div(mismatch, p.span)
+    reps = fld(mismatch, p.span)
     t_start -= reps * p.span
     t_end -= reps * p.span
     mismatch = t_end - p.value.indexes[1]
@@ -155,7 +151,6 @@ function (p::RepeatingTimeSeriesParameterValue)(t::TimeSlice)
     ab = _search_overlap(p.value, t_start, t_end - reps * p.span)
     isempty(ab) && return nothing
     a, b = ab
-    reps == 0 && a > b ? b = a : nothing
     asum = sum(Iterators.filter(!isnan, p.value.values[a:end]))
     bsum = sum(Iterators.filter(!isnan, p.value.values[1:b]))
     alen = count(!isnan, p.value.values[a:end])
