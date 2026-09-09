87b0725fbccff41eaa4da985881a3c53c18ed417
Antti Soininen <antti.soininen@vtt.fi>
2026-09-09 14:48:18 +0300

    Run garbage collection explicitly to collect a PyObject
    This seems to fix random crashes in SpineOpt unit tests.

--- src/api/db.jl
+++ src/api/db.jl
@@ -917,14 +917,18 @@ function _run_server_request(server_uri::URI, request::String, args::Tuple, kwar
     _do_run_server_request(server_uri, full_request)
 end
 function _run_server_request(dbh, request::String, args::Tuple, kwargs::Dict)
-    full_request = [request, args, kwargs, _client_version]
-    pybytes = Base.invokelatest(getproperty, @__MODULE__, :pybytes)
-    request = Base.invokelatest(pybytes, _encode(full_request))
-    io = IOBuffer()
-    str = Base.invokelatest(_handle_request, dbh, request)
-    write(io, str)
-    answer = _decode(io)
-    _process_db_answer(answer)
+    try
+        full_request = [request, args, kwargs, _client_version]
+        pybytes = Base.invokelatest(getproperty, @__MODULE__, :pybytes)
+        pyrequest = Base.invokelatest(pybytes, _encode(full_request))
+        io = IOBuffer()
+        str = Base.invokelatest(_handle_request, dbh, pyrequest)
+        write(io, str)
+        answer = _decode(io)
+        _process_db_answer(answer)
+    finally
+        GC.gc()
+    end
 end
 
 function _do_run_server_request(server_uri::URI, full_request::Array; timeout=Inf)
